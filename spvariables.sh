b71486e89fbf59f2d3d68b6a5a85f1d62a643990
srikanth-865 <srikanthram87@gmail.com>
2026-06-07 08:45:43 +0530

    srikanth

--- spvariables.sh
+++ spvariables.sh
@@ -1,3 +1,6 @@
 echo "all variables passed to the script $1 $2 $3"
 echo "How many variables passed:$#"
 echo "script name is $0"
+echo "user name is : $user"
+echo "pwd is "$pwd"
+echo "home directory is -$home"
