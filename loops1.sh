39a5ddd84a236392494cf564d95d61f70051fa6d
srikanth-865 <srikanthram87@gmail.com>
2026-06-08 10:40:54 +0530

    srikanth

---
+++ loops1.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_DIR=/home/ec2-user/shell-logs
+LOGS_FILE="$LOGS_DIR/$0.log" # /home/ec2-user/shell-logs/10-logs.sh.log
+
+# Check root access or not
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root access"
+    exit 1
+fi
+
+# first arg -> what are you trying to install
+# second arg -> exit code
+VALIDATE(){
+    if [ $2 -ne 0 ]; then
+        echo "Installing $1 is ... FAILED" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo "Installing $1 is ... SUCCESS" | tee -a $LOGS_FILE
+    fi
+}
+
+
+for package in $@ 
+do
+echo "installing packages:$package"
+dnf list installed $package &>> $LOGS_FILE 
+if [ $? -ne 0];then
+dnf install  $package -y &>>$LOGS_FILE
+VALIDATE "installing$package" $?
+else
+
+echo "$package already installed ..skip"
+ fi
+done
+
+
