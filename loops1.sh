a05a2597b58793bed7edea538ba121e2eed6271c
srikanth-865 <srikanthram87@gmail.com>
2026-06-08 11:26:02 +0530

    srikanth

--- loops1.sh
+++ loops1.sh
@@ -6,6 +6,12 @@ LOGS_FILE="$LOGS_DIR/$0.log" # /home/ec2-user/shell-logs/10-logs.sh.log
 
 Timestamp=$(date "+%Y-%m-%d %H %M:%S")
 
+#clours adding to terminal
+R="\e[31m"
+g="\e[32m"
+y="\e[33m"
+N="\e[0m"
+
 # Check root access or not
 if [ $USERID -ne 0 ]; then
     echo "Please run this script with root access"
@@ -16,10 +22,10 @@ fi
 # second arg -> exit code
 VALIDATE(){
     if [ $2 -ne 0 ]; then
-        echo "$Timestamp Installing $1 is ... FAILED" | tee -a $LOGS_FILE
+        echo "$Timestamp Installing $1 is ... $R FAILED" | tee -a $LOGS_FILE
         exit 1
     else
-        echo "$Timestamp Installing $1 is ... SUCCESS" | tee -a $LOGS_FILE
+        echo "$Timestamp Installing $1 is ... $g SUCCESS" | tee -a $LOGS_FILE
     fi
 }
 
@@ -32,7 +38,7 @@ if [ $? -ne 0 ]; then
 dnf install  $package -y &>>$LOGS_FILE
 VALIDATE "installing$package" $?
 else
-echo "$timestamp [info] $package already installed ..skip"
+echo "$timestamp [info] $package already installed ..$y skip"
  fi
 done
 
