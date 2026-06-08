812862255a4f8c93f02e56d9274d4c56023cde54
srikanth-865 <srikanthram87@gmail.com>
2026-06-08 06:55:11 +0530

    srikanth

--- install-functions.sh
+++ install-functions.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
-
 userid=$(id -u)
-if  [ $userid -ne 0 ];then
+if  [   $userid -ne 0 ];then
 echo "Please run this script with root access"
 exit 1
 fi
 dnf list installed mysql
-
-if [ $? -eq 0 ]; then
-    echo "MySQL is already installed ... SKIPPING"
-else
-    echo "Installing MySQL"
-    dnf install mysql -y
-    VALIDATE MySQL $?  #here we are passing arguments $1=mysql & $2=$?
-fi
-
 VALIDATE()
 {
     if [ $2 -ne 0 ]; then
@@ -24,6 +14,14 @@ VALIDATE()
         echo "Installing $1 is ... SUCCESS"
     fi
 }
+
+if [ $? -eq 0 ]; then
+    echo "MySQL is already installed ... SKIPPING"
+else
+    echo "Installing MySQL"
+    dnf install mysql -y
+    VALIDATE MySQL $?  #here we are passing arguments $1=mysql & $2=$?
+fi
 
 dnf list installed nginx
 if [ $? -eq 0 ]; then
