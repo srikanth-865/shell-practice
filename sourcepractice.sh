705073c7fe2b2e27b14950448885452b27775ce5
srikanth-865 <srikanthram87@gmail.com>
2026-06-12 11:25:42 +0530

    srikanth

--- sourcepractice.sh
+++ sourcepractice.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "i am in file 1"
-sh source.sh  #its a file 2 data go to file 2 and read lines
+source ./source.sh  #its a file 2 data go to file 2 and read lines
 echo "PID of script-1: $$"
