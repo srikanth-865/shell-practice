6c770bcf36fd07af569ac6f88f453b7f2e2b6ee2
srikanth-865 <srikanthram87@gmail.com>
2026-06-12 11:14:22 +0530

    srikanth

--- source.sh
+++ source.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "i am in source 2 file"
+echo "i am in source 2 file taking input from sourcepractice file"
--- sourcepractice.sh
+++ sourcepractice.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "i am in file 1"
-sh source.sh
+sh source.sh  #its a file 2 data go to file 2 and read lines
