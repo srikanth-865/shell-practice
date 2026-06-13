7b1d7992b8d302ce576e382233515ea63b111e1a
srikanth-865 <srikanthram87@gmail.com>
2026-06-13 13:44:50 +0530

    srikanth

--- logscleanup.sh
+++ logscleanup.sh
@@ -3,7 +3,7 @@
 SOURCE_DIR=$1
 DAYS=${2:-14} # default 14 days
 
-if [ -z "$SOURCE_DIR" ]; then
+if [ -z "$SOURCE_DIR" ]; then  #directory is present or not
     echo "ERROR:: Missing parameters"
     echo "USAGE:: $0 <source-dir> [days(optional default to 14)]"
     exit 1
