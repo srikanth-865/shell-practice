0a99e7a3a26e8d5d4e3217f2ff2978a2df3b2d5c
srikanth-865 <srikanthram87@gmail.com>
2026-06-13 17:03:15 +0530

    srikanth

--- diskusage.sh
+++ diskusage.sh
@@ -9,7 +9,7 @@ do
     PARTITION=$( echo $line | awk '{print $7}' )
 
     if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
-        MESSAGE+="High Disk Usage on $PARTITION: $USAGE <br>"
+        MESSAGE+="High Disk Usage on $PARTITION: $USAGE \n"
     fi
 done <<< "$DISK_USAGE"
 
