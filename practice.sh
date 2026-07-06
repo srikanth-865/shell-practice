20b5fed22055696e28c7923acccb0d9ffe4c7545
unknown <srikanthram87@gmail.com>
2026-07-06 08:45:37 +0530

    oil and karam

--- practice.sh
+++ practice.sh
@@ -1,4 +1,7 @@
  #!/bin/bash
  
-  today=$(date)
-  echo "todays date&time is $today"
+  START_TIME=$(date)
+  sleep 3
+  STOP_TIME=$(date)
+  TOTAL_TIME=(($START_TIME-$STOP_TIME))
+  echo "total time for execute in $TOTAL_TIME seconds"
