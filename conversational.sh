6a61d51235959b04e93f7e8c6b62ef986665771a
unknown <srikanthram87@gmail.com>
2026-07-06 08:19:38 +0530

    oil and karam

--- conversational.sh
+++
@@ -1,9 +0,0 @@
- #!/bin/bash
-
-PERSON1=Russia
-PERSON2=Ukraine
-
-echo "$PERSON1: Hey Ukraine, we are going blast you!"
-echo "$PERSON2: Yes, please. you are welcome"
-echo "$PERSON1: Hey $PERSON2 I am serious"
-echo "$PERSON2: Hey $PERSON1, we are also serious"
---
+++ practice.sh
@@ -0,0 +1,6 @@
+ #!/bin/bash
+ person1= $1
+ person2 = $2
+
+ echo "$person1: i am from pdp"
+ echo "$person2: i am from dmd
