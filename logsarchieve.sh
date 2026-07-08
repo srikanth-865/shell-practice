b43904b4c7b536be78011affe3896e1daf707d2a
unknown <srikanthram87@gmail.com>
2026-07-08 13:21:55 +0530

    oil&karam

--- logsarchieve.sh
+++ logsarchieve.sh
@@ -9,3 +9,10 @@ echo "backup is done"
 else
 echo "backup is not done"
 fi
+echo "if backup is done if we delete our old files var/log/roboshop logs by using while loop line by line"
+
+while IFS= read -r line
+do
+echo "files are deleting"
+rm -f $file
+done <<< "$file"
