64d9e9a1df94cb04bab4b813a5be1f538616eca5
unknown <srikanthram87@gmail.com>
2026-07-08 14:32:30 +0530

    oil&karam

--- errorcount.sh
+++ errorcount.sh
@@ -2,7 +2,7 @@
 logfile=/var/log
 errorcount=$(grep -ri "error" $logfile | wc -l)
 if [ "$errorcount" -gt 0 ];then
-echo " error count is:$errorcount is more than 0 "
+echo "count is:$errorcount is more than 0 "
 else
 echo "no errors found "
 fi
