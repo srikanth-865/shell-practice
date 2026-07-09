8babea18d15b4b9bd696abdf6f491483787dad63
unknown <srikanthram87@gmail.com>
2026-07-09 10:10:14 +0530

    oil karam

--- assignment11.sh
+++ assignment11.sh
@@ -3,16 +3,16 @@ LOGS_FOLDER="/var/log/tmp/script"
 sudo mkdir -p $LOGS_FOLDER
 sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
 sudo chmod -R 755 $LOGS_FOLDER
-LOGS_FILE="$LOGS_FOLDER.log"
+LOGS_FILE="$LOGS_FOLDER/script.log"
 ls "$LOGS_FOLDER" 1>> "$LOGS_FILE"
 touch "$LOGS_FOLDER/srikanth.log"  
 echo "created srikanth.log" 1>> "$LOGS_FILE"
 touch "$LOGS_FOLDER/sk.log"   
 echo "created sk.log" 1>> "$LOGS_FILE"
- ls $LOGS_FOLDER 1>> "$LOGS_FILE"
+ ls $LOGS_FOLDER 1>> "$LOGS_FILE
 
 Error_folder="/var/log/script-errors"
-sudo mkdir $Error_folder
+sudo mkdir -p $Error_folder
 sudo chown -R ec2-user:ec2-user "$Error_folder"
 sudo chmod -R 755 "$Error_folder"
 ERRORLOGS_FILE="$Error_folder/scripts.log"
