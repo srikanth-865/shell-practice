ce53b712053b96bc19a9f567d699f81c64bbae68
unknown <srikanthram87@gmail.com>
2026-07-09 09:33:50 +0530

    oil karam

--- assignment11.sh
+++ assignment11.sh
@@ -4,6 +4,13 @@ sudo mkdir -p $LOGS_FOLDER
 sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
 sudo chmod -R 755 $LOGS_FOLDER
 LOGS_FILE="$LOGS_FOLDER/$0.log"
+
+Error_folder="/var/log/script-errors"
+sudo mkdir $Error_folder
+sudo chown -R ec2-user:ec2-user $Error_folder
+sudo chmod -R 755 $Error_folder
+ERRORLOGS_FILE="$Error_folder.log"
+
 echo "hello" 1>> $LOGS_FILE
-ls ljnkjl 2>> $LOGS_FILE
+ls ljnkjl 2>> $ERRORLOGS_FILE
 ls /tmp ls /fakedir  date  &>> $LOGS_FILE
