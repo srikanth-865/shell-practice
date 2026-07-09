8b64773b5b531a0d44aeb496e1b86bb307ca655f
unknown <srikanthram87@gmail.com>
2026-07-09 07:30:58 +0530

    oil karam

--- assignment8.sh
+++ assignment8.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 today_date=$(date +%A | awk '{print$1f}')
-if [ "$today_date" == "Sunday"] || [ "$today_date" == "Saturday"]
+if [ "$today_date" == "Sunday"] || [ "$today_date" == "Saturday"]; then
 echo "Weekend!Enjoy your holiday"
-elif [ "$today_date" == "Monday"]
+elif [ "$today_date" == "Monday"];then
 echo "Start of the week. Let's go!"
 else
 echo "Weekday. Go to school/work"
