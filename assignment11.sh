ee248eabdb35de27107fa664f7e12e6934e227b7
unknown <srikanthram87@gmail.com>
2026-07-09 09:25:57 +0530

    oil karam

--- assignment11.sh
+++ assignment11.sh
@@ -4,6 +4,6 @@ sudo mkdir -p $LOGS_FOLDER
 sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
 sudo chmod -R 755 $LOGS_FOLDER
 LOGS_FILE="$LOGS_FOLDER/$0.log"
-echo "hello" 1> $log_file
-ls ljnkjl 2> $log_file
-ls /tmp ls /fakedir  date  &> $log_file
+echo "hello" 1>> $log_file
+ls ljnkjl 2>> $log_file
+ls /tmp ls /fakedir  date  &>> $log_file
